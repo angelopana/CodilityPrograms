c70c6bc2c6c9b7f3007dbea6a1e32da49a6bf659
angelo <angelo2.0ken@gmail.com>
2020-02-25 18:57:30 -0700

    LeetCode 977

---
+++ squares_Of_A_Sorted_Array.go
@@ -0,0 +1,59 @@
+package main
+
+/*
+Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
+
+Example 1:
+
+Input: [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+
+Example 2:
+
+Input: [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+
+*/
+
+func square(num int) int {
+	num = num * num
+	return num
+}
+
+//quickSort
+func sort(arr []int, start, end int) {
+	if (end - start) < 1 {
+		return
+	}
+
+	pivot := arr[end]
+	splitIndex := start
+
+	for i := start; i < end; i++ {
+		if arr[i] < pivot {
+			temp := arr[splitIndex]
+
+			arr[splitIndex] = arr[i]
+			arr[i] = temp
+
+			splitIndex++
+		}
+	}
+
+	arr[end] = arr[splitIndex]
+	arr[splitIndex] = pivot
+
+	sort(arr, start, splitIndex-1)
+	sort(arr, splitIndex+1, end)
+}
+
+func sortedSquares(A []int) []int {
+	var temp []int
+	for _, number := range A {
+		temp = append(temp, square(number))
+	}
+
+	sort(temp, 0, len(temp)-1)
+
+	return temp
+}
