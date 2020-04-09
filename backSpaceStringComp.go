65fe1ae174b0f246e160eb8b991506ed0c95aa0a
angelo <angelo2.0ken@gmail.com>
2020-04-09 11:41:28 -0700

    Leetcode 844. Backspace String Compare

---
+++ backSpaceStringComp.go
@@ -0,0 +1,70 @@
+package main
+
+import "fmt"
+
+/*
+
+Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+
+Example 1:
+
+Input: S = "ab#c", T = "ad#c"
+Output: true
+Explanation: Both S and T become "ac".
+
+Example 2:
+
+Input: S = "ab##", T = "c#d#"
+Output: true
+Explanation: Both S and T become "".
+
+Example 3:
+
+Input: S = "a##c", T = "#a#c"
+Output: true
+Explanation: Both S and T become "c".
+
+
+*/
+
+func backspaceCompare(S string, T string) bool {
+
+	var s1 []rune
+	var s2 []rune
+
+	s1 = addToStack(S)
+	s2 = addToStack(T)
+
+	fmt.Println(string(s2))
+
+	if string(s1) == string(s2) {
+		return true
+	}
+
+	return false
+}
+
+func addToStack(s string) []rune {
+
+	var stack []rune
+	for _, j := range s {
+		if j == '#' {
+			if len(stack) != 0 {
+				top := len(stack) - 1
+				stack = stack[:top] // pop
+			}
+
+		} else {
+			stack = append(stack, j)
+		}
+	}
+	return stack
+}
+
+func main() {
+
+	s := "a##c"
+	t := "#a#c"
+
+	fmt.Println(backspaceCompare(s, t))
+}
